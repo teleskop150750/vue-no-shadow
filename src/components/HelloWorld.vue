a225d59c6f0d4b62936258b84957b5e4b7d5b52a
teleskop150750 <teleskop150750@gmail.com>
2023-05-02 17:44:40 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  msg: string
+}>()
+
+
+function foo() {
+  const props = 3
+
+  return props
+}
+</script>
+
+
+
+<template>
+  <div class="greetings">
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
